3d6f7cded03da884bc803f1b9eb6c58cae2758ee
LudovicMarquis <ludovic.marquis@he-arc.ch>
2024-10-14 16:13:48 +0200

    Pour le fichier ProductMapper : - Ajout des méthodes CRUD (create, read, update, delete) pour la persistance des produits - Adaptation des requêtes SQL aux colonnes de la table Produit (numero, fk_resto, prix_unitaire, nom, description) - Intégration de la méthode auxiliaire pour récupérer un restaurant via RestaurantMapper
    Pour le fichier ProductCLI :
    - Ajout de l'affichage des produits d'un restaurant avec nom, prix, et description
    - Implémentation de la sélection d'un produit via l'interface utilisateur

--- src/main/java/ch/hearc/ig/orderresto/presentation/ProductCLI.java
+++ src/main/java/ch/hearc/ig/orderresto/presentation/ProductCLI.java
@@ -4,6 +4,8 @@ import ch.hearc.ig.orderresto.business.Order;
 import ch.hearc.ig.orderresto.business.Product;
 import ch.hearc.ig.orderresto.business.Restaurant;
 
+import java.util.ArrayList;
+import java.util.List ;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -11,12 +13,16 @@ public class ProductCLI extends AbstractCLI {
 
     public Product getRestaurantProduct(Restaurant restaurant) {
         this.ln(String.format("Bienvenue chez %s. Choisissez un de nos produits:", restaurant.getName()));
-        Object[] products = restaurant.getProductsCatalog().toArray();
-        for (int i = 0 ; i < products.length ; i++) {
-            Product product = (Product) products[i];
-            this.ln(String.format("%d. %s", i, product));
+
+        // Récupérer la liste des produits du restaurant
+        List<Product> products = new ArrayList<>(restaurant.getProductsCatalog());
+
+        for (int i = 0; i < products.size(); i++) {
+            Product product = products.get(i);
+            this.ln(String.format("%d. %s - %.2f CHF (%s)", i, product.getName(), product.getUnitPrice(), product.getDescription()));
         }
-        int index = this.readIntFromUser(products.length - 1);
-        return (Product) products[index];
+
+        int index = this.readIntFromUser(products.size() - 1);  // Demande à l'utilisateur de choisir un produit
+        return products.get(index);  // Retourne le produit sélectionné
     }
 }
