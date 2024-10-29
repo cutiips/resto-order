7b90acec6b9ffb717ded8c2ef6d2c1e7b925597d
cutiips <jonathan.curty@he-arc.ch>
2024-10-29 21:59:51 +0100

    ✨ Ajout de ProductPersistenceException pour gérer les erreurs de persistance des produits - Création d'une exception personnalisée pour encapsuler les erreurs liées à la persistance des produits. - Facilite la gestion des erreurs lors des opérations CRUD dans ProductMapper.

---
+++ src/main/java/ch/hearc/ig/orderresto/exceptions/ProductPersistenceException.java
@@ -0,0 +1,7 @@
+package ch.hearc.ig.orderresto.exceptions;
+
+public class ProductPersistenceException extends Exception {
+    public ProductPersistenceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
