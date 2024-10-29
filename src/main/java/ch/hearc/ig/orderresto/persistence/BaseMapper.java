da9fd8954f7374724e4130724d9835c726563eab
cutiips <jonathan.curty@he-arc.ch>
2024-10-29 19:30:56 +0100

    🏗️ Ajout de la classe BaseMapper - Mise en place d'une classe abstraite pour gérer la connexion à la base de données. - Chargement des propriétés de configuration (URL, nom d'utilisateur et mot de passe) depuis le fichier config.properties. - Implémentation de la méthode getConnection pour établir une connexion avec la base de données.

---
+++ src/main/java/ch/hearc/ig/orderresto/persistence/BaseMapper.java
@@ -0,0 +1,40 @@
+package ch.hearc.ig.orderresto.persistence;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public abstract class BaseMapper {
+    protected String url;
+    protected String username;
+    protected String password;
+
+    public BaseMapper() {
+        loadProperties();
+    }
+
+    protected void loadProperties() {
+        Properties prop = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }
+            prop.load(input);
+            url = prop.getProperty("db.url");
+            username = prop.getProperty("db.username");
+            password = prop.getProperty("db.password");
+        } catch (IOException ex) {
+            System.err.println("Error loading properties: " + ex.getMessage());
+            throw new RuntimeException("Database configuration error", ex);
+        }
+    }
+
+    protected Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
+}
+
