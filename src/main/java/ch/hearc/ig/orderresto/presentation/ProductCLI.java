3f1fab47b4bfefc3a7203ab7ca3ef03806bf430a
cutiips <jonathan.curty@he-arc.ch>
2024-10-29 21:54:05 +0100

    🔄 Refactorisation de ProductCLI pour intégrer la persistance des produits - Ajout de ProductMapper pour récupérer les produits à partir de la base de données. - Vérification de la disponibilité des produits pour chaque restaurant. - Gestion des cas où aucun produit n'est disponible.

--- src/main/java/ch/hearc/ig/orderresto/presentation/ProductCLI.java
+++ src/main/java/ch/hearc/ig/orderresto/presentation/ProductCLI.java
@@ -1,28 +1,32 @@
 package ch.hearc.ig.orderresto.presentation;
 
-import ch.hearc.ig.orderresto.business.Order;
 import ch.hearc.ig.orderresto.business.Product;
 import ch.hearc.ig.orderresto.business.Restaurant;
+import ch.hearc.ig.orderresto.exceptions.ProductPersistenceException;
+import ch.hearc.ig.orderresto.persistence.ProductMapper;
 
-import java.util.ArrayList;
 import java.util.List ;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
 
 public class ProductCLI extends AbstractCLI {
-
-    public Product getRestaurantProduct(Restaurant restaurant) {
+    public Product getRestaurantProduct(Restaurant restaurant) throws ProductPersistenceException {
         this.ln(String.format("Bienvenue chez %s. Choisissez un de nos produits:", restaurant.getName()));
 
-        // Récupérer la liste des produits du restaurant
-        List<Product> products = new ArrayList<>(restaurant.getProductsCatalog());
+        ProductMapper productMapper = new ProductMapper();
+        // récupérer les produits du restaurant
+        List<Product> products = productMapper.findProductsByRestaurant(restaurant);
+
+        if (products.isEmpty()) {
+            this.ln("Aucun produit disponible pour ce restaurant.");
+            return null;
+        }
 
         for (int i = 0; i < products.size(); i++) {
             Product product = products.get(i);
             this.ln(String.format("%d. %s - %.2f CHF (%s)", i, product.getName(), product.getUnitPrice(), product.getDescription()));
         }
 
-        int index = this.readIntFromUser(products.size() - 1);  // Demande à l'utilisateur de choisir un produit
-        return products.get(index);  // Retourne le produit sélectionné
+        int index = this.readIntFromUser(products.size() - 1);
+        return products.get(index);
     }
 }
