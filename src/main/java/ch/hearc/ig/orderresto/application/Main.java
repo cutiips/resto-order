23a48871e33fb2a6a254bbdc4c6d0f0b6d753867
cutiips <jonathan.curty@he-arc.ch>
2024-10-29 20:14:19 +0100

    🔧 Ajout de la gestion des exceptions dans la classe Main - Modification de la méthode main() pour déclarer ProductPersistenceException comme exception lancée. - Préparation pour une gestion d'erreur améliorée lors de l'exécution de MainCLI.

--- src/main/java/ch/hearc/ig/orderresto/application/Main.java
+++ src/main/java/ch/hearc/ig/orderresto/application/Main.java
@@ -1,12 +1,13 @@
 package ch.hearc.ig.orderresto.application;
 
+import ch.hearc.ig.orderresto.exceptions.ProductPersistenceException;
 import ch.hearc.ig.orderresto.presentation.MainCLI;
 
 import java.sql.SQLException;
 
 public class Main {
 
-  public static void main(String[] args) throws SQLException {
+  public static void main(String[] args) throws SQLException, ProductPersistenceException {
 
     (new MainCLI()).run();
   }
