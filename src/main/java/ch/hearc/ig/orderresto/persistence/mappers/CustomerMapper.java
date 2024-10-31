f1ec481cefe197064cd0df526df9275645938d45
cutiips <jonathan.curty@he-arc.ch>
2024-10-31 18:59:44 +0100

    ✨ Ajout de la gestion de l'ID généré pour Customer et amélioration du cache dans CustomerMapper

--- src/main/java/ch/hearc/ig/orderresto/persistence/mappers/CustomerMapper.java
+++ src/main/java/ch/hearc/ig/orderresto/persistence/mappers/CustomerMapper.java
@@ -1,6 +1,7 @@
 package ch.hearc.ig.orderresto.persistence.mappers;
 
 import ch.hearc.ig.orderresto.business.Customer;
+import ch.hearc.ig.orderresto.persistence.IdentityMap;
 import ch.hearc.ig.orderresto.persistence.exceptions.CustomerPersistenceException;
 import ch.hearc.ig.orderresto.persistence.utils.CustomerUtils;
 
@@ -11,20 +12,32 @@ import java.sql.SQLException;
 import java.util.Optional;
 
 public class CustomerMapper extends BaseMapper<Customer> {
-
     public void insert(Customer customer) throws CustomerPersistenceException {
         String query = "INSERT INTO CLIENT (telephone, email, nom, code_postal, localite, rue, num_rue, pays, est_une_femme, prenom, forme_sociale, type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
         try (Connection conn = getConnection();
-             PreparedStatement stmt = conn.prepareStatement(query)) {
+             PreparedStatement stmt = conn.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS)) {
 
             CustomerUtils.setPreparedStatementForCustomer(stmt, customer);
             stmt.executeUpdate();
+
+            // Récupérer l'ID généré par la base de données
+            try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    customer.setId(generatedKeys.getLong(1));
+                } else {
+                    throw new SQLException("Échec de l'insertion du client, aucun ID généré.");
+                }
+            }
+
+            // Ajouter au cache avec l'ID mis à jour
             addToCache(customer.getId(), customer);
+
         } catch (SQLException e) {
             throw new CustomerPersistenceException("Erreur lors de l'insertion du client", e);
         }
     }
+
 
     public Customer read(String email) throws CustomerPersistenceException {
         String query = "SELECT * FROM CLIENT WHERE email = ?";
@@ -51,6 +64,7 @@ public class CustomerMapper extends BaseMapper<Customer> {
     public Customer researchById(Long id) throws CustomerPersistenceException {
         Optional<Customer> cachedCustomer = findInCache(id);
         if (cachedCustomer.isPresent()) {
+            System.out.println("Customer found in cache: " + id);
             return cachedCustomer.get();
         }
 
@@ -62,19 +76,27 @@ public class CustomerMapper extends BaseMapper<Customer> {
 
             stmt.setLong(1, id);
             ResultSet rs = stmt.executeQuery();
+            System.out.println("1");
 
             if (rs.next()) {
                 customer = CustomerUtils.mapCustomerFromResultSet(rs);
-                if (customer != null) {
-                    addToCache(id, customer);
+                System.out.println("2");
+                if (customer != null && customer.getId() != null) {
+                    System.out.println("3");
+                    addToCache(customer.getId(), customer);
+                    System.out.println("Customer ajouté au cache avec ID : " + customer.getId());
                 }
+
             }
+            System.out.println("4");
         } catch (SQLException e) {
             throw new CustomerPersistenceException("Erreur lors de la recherche du client par ID", e);
         }
 
         return customer;
     }
+
+
 
     public void update(Customer customer) throws CustomerPersistenceException {
         String query = "UPDATE CLIENT SET telephone = ?, nom = ?, code_postal = ?, localite = ?, rue = ?, num_rue = ?, pays = ?, est_une_femme = ?, prenom = ?, forme_sociale = ? WHERE email = ?";
